b16aae74110a7a3f75d928681ccd942ea646e0ed
masaki nakamura <gmk4n0n@gmail.com>
2023-01-06 05:13:23 +0900

    installing dependencies of Firebase using swift package manager

---
+++ IphoneIpadFirebase/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  IphoneIpadFirebase
+//
+//  Created by masaki on 2023/01/06.
+//
+
+import UIKit
+import Firebase
+
+class AppDelegate : NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
